914e122bdb64b41bede7da99504b4ea6547f6b6b
Francisco Salcines <salcines@outlook.es>
2022-11-13 01:07:54 +0100

    Create a new project and work with nuget packages dependencies

--- NetConfStudentChallenge/Program.cs
+++ NetConfStudentChallenge/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Humanizer;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Quantities: ");
+HumanizerQuantities();
+Console.WriteLine($"\nDate/Time manipulation: ");
+HumanizerDates();
+
+static void HumanizerQuantities()
+{
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(3));
+    Console.WriteLine("case".ToQuantity(0));
+}
+
+static void HumanizerDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
+
